29c6402e234ef8b338c813bbc27d4b80821eced6
iosonospace <s1110054@studenti.univpm.it>
2024-01-08 17:21:51 +0100

    sdrogo

--- Racing game (AGD)/Assets/Script/CountdownScript.cs
+++ Racing game (AGD)/Assets/Script/CountdownScript.cs
@@ -14,6 +14,7 @@ public class CountdownScript : MonoBehaviour
     void Start()
     {
         StartCoroutine(CountStart());
+        TimerGiro.SetActive(false);
         CarUserControl.GetComponent<ControllerAuto>().enabled = false;
     }
 
