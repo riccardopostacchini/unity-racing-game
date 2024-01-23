b0b0f6077c0acdafcda78afe4d23fd6a452d8eb6
riccardopostacchini <s1098289@studenti.univpm.it>
2024-01-23 10:56:25 +0100

    Implementazione menu di pausa

--- Racing game (AGD)/Assets/Script/ControllerAuto.cs
+++ Racing game (AGD)/Assets/Script/ControllerAuto.cs
@@ -18,22 +18,24 @@ public class ControllerAuto : MonoBehaviour
     
     void Update()
     {
+        if (!MenuPausa.inPausa)
+        {
+            // Moving
+            MoveForce += transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += MoveForce * Time.deltaTime;
 
-        // Moving
-        MoveForce += transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position += MoveForce * Time.deltaTime;
+            // Steering
+            float steerInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
 
-        // Steering
-        float steerInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
+            // Drag and max speed limit
+            MoveForce *= Drag;
+            MoveForce = Vector3.ClampMagnitude(MoveForce, MaxSpeed);
 
-        // Drag and max speed limit
-        MoveForce *= Drag;
-        MoveForce = Vector3.ClampMagnitude(MoveForce, MaxSpeed);
-
-        // Traction
-        Debug.DrawRay(transform.position, MoveForce.normalized * 3);
-        Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
-        MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.deltaTime) * MoveForce.magnitude;
+            // Traction
+            Debug.DrawRay(transform.position, MoveForce.normalized * 3);
+            Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
+            MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.deltaTime) * MoveForce.magnitude;
+        }
     }
 }
