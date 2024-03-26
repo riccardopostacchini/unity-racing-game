ad5a97c32ea9b15d0674fea573466a51a5a93c6b
riccardopostacchini <s1098289@studenti.univpm.it>
2024-03-26 12:28:55 +0100

    Fix trigger FuoriPista

--- Racing game (AGD)/Assets/Script/FuoriStrada.cs
+++ Racing game (AGD)/Assets/Script/FuoriStrada.cs
@@ -4,32 +4,46 @@ using UnityEngine;
 
 public class FuoriStrada : MonoBehaviour
 {
-    public GameObject FuoriPista;
+    // Riferimento al rigidbody dell'auto
     public Rigidbody Auto;
+
+    // Riferimento al collider del detector di strada
+    public Collider Detector;
+
+    // Valore di drag normale sull'asfalto
+    public float normalDrag = 0f;
+
+    // Valore di drag quando l'auto è fuori strada
+    public float offRoadDrag = 0.2f;
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("ColliderFuoriPista"))
+        if (other.CompareTag("FuoriPista"))
         {
-            Auto.drag = 0.7f;
-            Debug.Log("Velocita abbassata");
-        }
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("ColliderFuoriPista"))
-        {
-            Auto.drag = 0.7f;
-            Debug.Log("Velocita tenuta bassa");
+            // Se l'auto entra nel trigger della strada, imposta il drag normale
+            Auto.drag = offRoadDrag;
+            Debug.Log("Attrito aumentato");
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("ColliderFuoriPista"))
+        if (other.CompareTag("FuoriPista"))
         {
-            Auto.drag = 0.4f;
-            Debug.Log("Velocita normale");
+            // Se l'auto esce dal trigger della strada, imposta il drag maggiore
+            Auto.drag = normalDrag;
+            Debug.Log("Attrito normale");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("FuoriPista"))
+        {
+            // Se l'auto rimane nel trigger della strada, imposta il drag normale
+            Auto.drag = offRoadDrag;
+            Debug.Log("Attrito aumentato");
         }
     }
 }
+
